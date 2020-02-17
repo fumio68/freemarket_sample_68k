6cb6347eba4853563917f31af32349582aea10ce
palo08 <paralelo0131@gmail.com>
2020-02-17 20:19:51 +0900

    commentout_delete
    Delete comment out

--- app/assets/javascripts/sign-up.js
+++ app/assets/javascripts/sign-up.js
@@ -1,20 +1,15 @@
-
-//新規登録時  例外処理
-
 $(function(){
 	$(".main__box__contents__eight__middle").on('click',function(e){
-    e.preventDefault();                      //エラー文消さない
-    if(input_check()){                        //
-			return false;                           //親要素のイベントを発生させない
+    e.preventDefault();                     
+    if(input_check()){                        
+			return false;                           
 		}
 	});
 });
 
-// 入力内容チェックのための関数
 function input_check(){
 	let result = true;
 
-	// エラー用装飾のためのクラスリセット
 	$('.main__box__contents__one__form').removeClass("inp_error");
 	$('.main__box__contents__two__form').removeClass("inp_error");
 	$('.main__box__contents__three__form').removeClass("inp_error");
@@ -26,7 +21,6 @@ function input_check(){
 	$('.main__box__contents__seven__middle__month-box').removeClass("inp_error");
 	$('.main__box__contents__seven__middle__date-box').removeClass("inp_error");
   
-	// 入力エラー文をリセット
 	$("#nickname_error").empty();
 	$("#email_error").empty();
 	$("#password_error").empty();
@@ -35,10 +29,7 @@ function input_check(){
 	$(".main__box__contents__six__name-error__j-family").empty();
 	$(".main__box__contents__six__name-error__j-last").empty();
 	$('#b_year_error').empty();
-	// $('#b_month_error').empty();
-	// $('#b_day_error').empty();
 
-	// 入力内容セット
 	let nickname      =$(".main__box__contents__one__form").val();
 	let email         =$(".main__box__contents__two__form").val();
 	let password      =$(".main__box__contents__three__form").val();
@@ -50,17 +41,12 @@ function input_check(){
 	let b_month       =$(".main__box__contents__seven__middle__month-box").val();
 	let b_day         =$(".main__box__contents__seven__middle__date-box").val();
 
-
-	// 入力内容チェック
-
-	//ニックネーム
 	if(nickname == ""){
 		$("#nickname_error").html(" ニックネームを入力してください");
 		$(".main__box__contents__one__form").addClass("inp_error");
 		result = false;
 	}
 	
-	// メールアドレス
 	if(email == ""){
 		$("#email_error").html(" メールアドレスを入力してください。");
 		$(".main__box__contents__two__form").addClass("inp_error");
@@ -71,7 +57,6 @@ function input_check(){
 		result = false;
 	}
 
-	// パスワード
   if(password == ""){
 		$("#password_error").html(" パスワードを入力してください");
 		$(".main__box__contents__three__form").addClass("inp_error");
@@ -82,35 +67,30 @@ function input_check(){
 		result = false;
 	}
 
-	// name sei kanzi
 	if(nameseikanzi == ""){
 		$(".main__box__contents__five__name-error__family").html("姓を入力してください");
 		$(".main__box__contents__five__form__family").addClass("inp_error");
 		result = false;
 	}
 
-	//name mei kanzi
 	if(namemeikanzi == ""){
 		$(".main__box__contents__five__name-error__last").html("名を入力してください");
 		$(".main__box__contents__five__form__last").addClass("inp_error");
 		result = false;
 	}
 
-	// name sei kana
 	if(nameseikana == ""){
 		$(".main__box__contents__six__name-error__j-family").html("姓カナを入力してください");
 		$(".main__box__contents__six__form__j-family").addClass("inp_error");
 		result = false;
 	}
 
-	// name mei kana
 	if(namemeikana == ""){
 		$(".main__box__contents__six__name-error__j-last").html("名カナを入力してください");
 		$(".main__box__contents__six__form__j-last").addClass("inp_error");
 		result = false;
 	}
 
-	//b_year, b_month, b_day
 	if(b_year == "" && b_month == "" && b_day == ""){
 		$("#b_year_error").html("生年月日を入力してください");
 		$(".main__box__contents__seven__middle__year-box").addClass("inp_error");
