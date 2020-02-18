ca67f4d0c1d2df45a1710ee36eab49314d2ae453
erknssgrrmns <0wd38c1x1334w9j@gmail.com>
2020-02-18 12:33:39 +0900

    create sold decoration

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
     @item_images = ItemImage.all
     @user = User.find(1)
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def search_images(itemId)
-    result = Item_image.where(item_id: itemId)
-    # result = result.image
-    return result
-  end
 end
