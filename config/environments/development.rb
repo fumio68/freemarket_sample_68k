8c5c5bf81925c38fb69ab36ec884ee8b1263212a
Shin-tech25 <m.shin0509@gmail.com>
2020-02-19 07:47:24 +0900

    modified require key for production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  # require master key
-  config.require_master_key = true
+  
 end
