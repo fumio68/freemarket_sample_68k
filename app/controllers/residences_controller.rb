17cf41253f1bd76388e22ccf02908ed2ad14a6cd
Shin-tech25 <m.shin0509@gmail.com>
2020-02-19 12:37:28 +0900

    routing, default template

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -4,6 +4,9 @@ class ResidencesController < ApplicationController
     residence = Residence.create(residence_params)
   end
 
+  def show
+  end
+
   private
   def residence_params
     params.require(:residence).permit(:family_name, :last_name, :j_family_name, :j_last_name, :postcode, :prefecture, :city, :block)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :items, only: [:index,:new, :create, :show]
-  resources :users, only: [:index, :show] 
+  resources :users, only: [:index, :show] do
+    resources :residences
+  end
   root "items#index"
 end
