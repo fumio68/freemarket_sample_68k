b6a78d0f4d34349500ed8b5ee8917cd9f2843fca
fumio68 <rgkk.5123@gmail.com>
2020-02-19 17:22:09 +0900

    add strongParameter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,8 +11,20 @@ class ItemsController < ApplicationController
     @item.item_images.new
   end
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :content, :brand, :condition, :shipping, :shipping_area, :days_to_ship, :shipping_method, :status,category_ids: [], item_images_attributes: [:image]).merge(user_id: current_user.id)
   end
 
 
