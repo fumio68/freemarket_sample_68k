0d90c033c2cdf22c046b8e3b3ea593409750bd22
fumio68 <rgkk.5123@gmail.com>
2020-02-19 16:47:23 +0900

    add accepts_nested_attributes_for :images
    fields_forを使用するために必要な関係性を設定。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   # association
   belongs_to :user
-  has_many :item_images
   belongs_to :category
+  has_many :item_images
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   # validation
   validates :name,          presence: true
