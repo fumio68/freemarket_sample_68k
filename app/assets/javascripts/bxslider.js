0e37dbd98c3decdb12ffab4fd9d856d45c7a8a48
erknssgrrmns <0wd38c1x1334w9j@gmail.com>
2020-02-20 11:49:00 +0900

    slide image by bxslider

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -1,5 +1,13 @@
 $(function(){
+  var thumbWidth = 120;
+  var thumbHeight = 80;
+  var insert = '';
+  for (var i = 0; i < $('.main__show__content__top__itemInfo__image__ul li').length; i++) {
+      insert += '<span id = slide' + i + '> <a data-slide-index="' + i + '" href="#"><img src="' + $('.main__show__content__top__itemInfo__image__ul li').eq(i).children('img').attr('src') + '" width="' + thumbWidth + '" height="' + thumbHeight + '" class="thumb"/></a></span>';
+  };
+  $('.custom-thumb').append(insert);
   $('.main__show__content__top__itemInfo__image__ul').bxSlider({
-    pagerType: 'short',
- });
+    pagerCustom: '.custom-thumb',
+    controls: false
+  });
 });
