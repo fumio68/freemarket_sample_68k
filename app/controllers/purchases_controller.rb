24ff82d53d4b140e2f41dba95f0c8f7743911eb1
Shin-tech25 <m.shin0509@gmail.com>
2020-02-20 20:11:09 +0900

    .new_record

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,9 +2,9 @@ class PurchasesController < ApplicationController
   def index
   end
   def create
+
   end
   def show
-
     # @residence = Residence.find(current_user.id)
     if Residence.where(id: 1).exists?
       @residence = Residence.find(1)
