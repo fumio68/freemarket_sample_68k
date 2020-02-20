11b1c4458cf96b4b1fa29137184c24b2c5d73431
fumio68 <rgkk.5123@gmail.com>
2020-02-20 16:22:38 +0900

    add two jbuilder
     - items_newに記述したget_category_childrenメソッド、get_category_grandchildrenメソッドに対応するjbuilderファイルを作成。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,7 @@ class ItemsController < ApplicationController
     @parents = Category.order("id ASC").limit(13)
     # @user = User.find(1)
   end
+
   def new
     @item = Item.new
     @item.item_images.new
@@ -15,17 +16,19 @@ class ItemsController < ApplicationController
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
     end
-    def get_category_children
-      #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-    end
-
-    # 子カテゴリーが選択された後に動くアクション
-    def get_category_grandchildren
-      #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-      @category_grandchildren = Category.find("#{params[:child_id]}").children
-    end
   end
+
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
