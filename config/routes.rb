d91d7969cfb69e2775a803efb3f8c2f97abfb40a
fumio68 <rgkk.5123@gmail.com>
2020-02-20 14:12:47 +0900

    add Ajax route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :items, only: [:index,:new, :create, :show]
+  resources :items, only: [:index,:new, :create, :show] do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   resources :users, only: [:index, :show] 
   root "items#index"
 end
