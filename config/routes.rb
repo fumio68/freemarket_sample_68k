067bda5d7d742267785f9bd1e86123804e0921bc
shi-eng <kureko.nnv5@gmail.com>
2020-02-21 11:37:41 +0900

    [WIP]Payjp
    登録画面のマークアップ作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   devise_for :users
   resources :items ,only: [:index, :create, :show] do
     resources :purchases, only: [:index, :create, :show]
   end
   resources :users, only: [:index, :show] 
   root "items#index"
+  resources :card, only: [:index, :new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
