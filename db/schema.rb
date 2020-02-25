249e5159fa37811559552d4147b94cb5c38c3e3f
Shin <m.shin0509@gmail.com>
2020-02-25 18:38:04 +0900

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_25_063010) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
