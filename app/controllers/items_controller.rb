95d25aee7990b07f01fac7880a8088450f07dcfb
Shin <m.shin0509@gmail.com>
2020-02-25 21:08:43 +0900

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,6 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.order("created_at DESC")
     @item_images = ItemImage.all
-    if user_signed_in?
-      @user = User.find(current_user.id)
-    end
     @parents = Category.order("id ASC").limit(13)
   end
 
@@ -40,9 +37,6 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @parents = Category.order("id ASC").limit(13)
-    if user_signed_in?
-      @user = User.find(current_user.id)
-    end
   end
 
 
