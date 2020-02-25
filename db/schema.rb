8e9c423aeaeb45d2f116d0a09af6169a37a81d51
palo08 <paralelo0131@gmail.com>
2020-02-25 17:20:27 +0900

    path_fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_051303) do
+ActiveRecord::Schema.define(version: 2020_02_25_073031) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
