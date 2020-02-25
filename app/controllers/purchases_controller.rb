a0a211cfceebb27eff99ab83a27f64830bc19d7a
Shin <m.shin0509@gmail.com>
2020-02-25 14:48:46 +0900

    modified

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,7 @@ class PurchasesController < ApplicationController
   def index
     @itemId = params[:item_id]
     @purchaseId = params[:id]
-    if Residence.where(user_id: current_user.id).exists?
+    if current_user.residences.present?
       @residence = Residence.find_by(user_id: current_user.id)
     else
       @residence = Residence.new
@@ -19,7 +19,7 @@ class PurchasesController < ApplicationController
     
     @parents = Category.order("id ASC").limit(13)
     @itemId = params[:item_id]
-    if Residence.where(user_id: current_user.id).exists?
+    if current_user.residences.present?
       @residence = Residence.find_by(user_id: current_user.id)
     else
       @residence = Residence.new()
@@ -27,7 +27,7 @@ class PurchasesController < ApplicationController
   end
 
   def post_purchase_modify
-    if Residence.where(user_id: current_user.id).exists?
+    if current_user.residences.present?
       target = Residence.where(user_id: current_user.id)
       target.update(residence_params)
       redirect_to item_purchases_path(item_id: params[:item_id])
