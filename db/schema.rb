ddf02f74bd3a33bf0cff88ad360a21282d4c7ac1
palo08 <paralelo0131@gmail.com>
2020-02-25 17:05:29 +0900

    schema_fix

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_073031) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "shipping"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
