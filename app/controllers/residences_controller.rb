2aeb92857ffde3f71af492f1f2c8bf569aa15839
Shin.M <59676674+Shin-tech25@users.noreply.github.com>
2020-02-25 14:09:25 +0900

    Revert "Revert "[WIP]商品購入機能（サーバーサイド）配送先情報変更""

--- app/controllers/residences_controller.rb
+++ app/controllers/residences_controller.rb
@@ -1,14 +1,23 @@
 class ResidencesController < ApplicationController
 
   def create
-    @residence = Residence.create(residence_params)
+    if Residence.where(user_id: current_user.id).exists?
+      target = Residence.where(user_id: current_uesr.id)
+      target.update(residence_params)
+      redirect_back(fallback_location: root_path)
+    else
+      @residence = Residence.new(residence_params)
+      @residence.save!
+      redirect_back(fallback_location: root_path)
+    end
   end
 
-  def show
+  def index
     @parents = Category.order("id ASC").limit(13)
+
     @user = User.find(current_user.id)
     if Residence.where(user_id: current_user.id).exists?
-      @residence = Residence.find(current_user.id)
+      @residence = Residence.find_by(user_id: current_user.id)
     else
       @residence = Residence.new()
     end
@@ -16,6 +25,6 @@ class ResidencesController < ApplicationController
 
   private
   def residence_params
-    params.require(:residence).permit(:family_name, :last_name, :j_family_name, :j_last_name, :postcode, :prefecture, :city, :block)
+    params.require(:residence).permit(:family_name, :last_name, :j_family_name, :j_last_name, :postcode, :prefecture, :city, :block).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items, only: [:index,:new, :create, :show] do
-    resources :purchases, only: [:index, :create, :show]
+    resources :purchases, only: [:index, :create, :show] do
+      collection do
+        get 'get_purchase_modify'
+        post 'post_purchase_modify'
+      end
+    end
   end
   resources :users, only: [:index, :show] do
-    resources :residences, only: [:create, :show]
+    resources :residences, only: [:create, :index]
     resources :card, only: [:new, :index] do
       collection do
         post 'pay', to: 'card#pay'
@@ -15,4 +20,5 @@ Rails.application.routes.draw do
   root "items#index"
   
   resources :categories, only: [:index]
+  
 end
