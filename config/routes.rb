a02c9ab756eb30ea2f568f93c13df11e902f0742
Shin.M <59676674+Shin-tech25@users.noreply.github.com>
2020-02-25 14:09:06 +0900

    Revert "[WIP]商品購入機能（サーバーサイド）配送先情報変更"

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items, only: [:index,:new, :create, :show] do
-    resources :purchases, only: [:index, :create, :show] do
-      collection do
-        get 'get_purchase_modify'
-        post 'post_purchase_modify'
-      end
-    end
+    resources :purchases, only: [:index, :create, :show]
   end
   resources :users, only: [:index, :show] do
-    resources :residences, only: [:create, :index]
+    resources :residences, only: [:create, :show]
     resources :card, only: [:new, :index] do
       collection do
         post 'pay', to: 'card#pay'
@@ -20,5 +15,4 @@ Rails.application.routes.draw do
   root "items#index"
   
   resources :categories, only: [:index]
-  
 end
