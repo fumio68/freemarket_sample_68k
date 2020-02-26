752c813b5829033af5d37be808476f9769427a8e
Shin <m.shin0509@gmail.com>
2020-02-26 17:48:38 +0900

    stylesheets, get targetIndex

--- app/assets/javascripts/post_images.js
+++ app/assets/javascripts/post_images.js
@@ -21,16 +21,23 @@ $(function(){
   fileIndex.splice(0, lastIndex);
   $('.hidden-destroy').hide();
 
-  $('#image-box').on('change', '.js-file', function(e) {
+  $('#image-box').on('change', '.js-file_group__hidden', function(e) {
     // 画像と削除ボタンを追加して、file-fieldの大きさを変更できるような処理にする。
     // 画像のイメージを表示できるような処理をつける。
+    // console.log('Hello World.');
     var targetIndex = $(this).parent().data('index');
+
+    console.log('$(this)', $(this));
+    console.log('$(this).parent()', $(this).parent());
+    console.log('targetIndex', targetIndex);
+
     var file = e.target.files[0];
     var blobUrl = window.URL.createObjectURL(file);
+    console.log(blobUrl);
+    /*
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
-      console.log('bind(1)');
     } else {  // 新規画像追加の処理
       console.log('bind(2)');
       $('#previews').append(buildImg(targetIndex, blobUrl));
@@ -40,6 +47,7 @@ $(function(){
       // 末尾の数に1足した数を追加する
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }
+    */
   });
 
   $('#image-box').on('click', '.js-remove', function() {
