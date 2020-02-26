a2f3f577b835cd92438f594db126937b69540eb5
Shin <m.shin0509@gmail.com>
2020-02-26 18:19:31 +0900

    commit

--- app/assets/javascripts/post_images.js
+++ app/assets/javascripts/post_images.js
@@ -2,6 +2,7 @@ $(function(){
   // function
   function buildFileField(index){
     var html = `<div data-index="${index}" class="js-file_group">
+
                   <input class="js-file" type="file"
                   name="item[item_images_attributes][${index}][image]"
                   id="item_item_images_attributes_${index}_image">
@@ -10,7 +11,7 @@ $(function(){
     return html;
   }
   function buildImg(index, url){
-    var html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    var html = `<img data-index="${index}" src="${url}" width="118px" height="118px">`;
     return html;
   }
 
@@ -19,7 +20,7 @@ $(function(){
   // 既に使われているindexを除外
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
-  $('.hidden-destroy').hide();
+  // $('.hidden-destroy').hide();
 
   $('#image-box').on('change', '.js-file_group__hidden', function(e) {
     // 画像と削除ボタンを追加して、file-fieldの大きさを変更できるような処理にする。
@@ -34,23 +35,28 @@ $(function(){
     var file = e.target.files[0];
     var blobUrl = window.URL.createObjectURL(file);
     console.log(blobUrl);
-    /*
+    
     // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {  // 新規画像追加の処理
-      console.log('bind(2)');
-      $('#previews').append(buildImg(targetIndex, blobUrl));
+      // console.log('Hello.');
+      $('#preview-box').append(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(fileIndex[0]));
+      $('#file-box').empty();
+      $('#file-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       // 末尾の数に1足した数を追加する
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+      
     }
-    */
+    
   });
 
   $('#image-box').on('click', '.js-remove', function() {
+    console.log('remove');
+    console.log($(this));
+    console.log($(this).parent());
     $(this).parent().remove();
     // 画像入力欄が0個にならないようにする
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
