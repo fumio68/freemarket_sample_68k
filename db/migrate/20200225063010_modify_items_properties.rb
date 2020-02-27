4579aae372bd8e543114ae90f56cea339b5d5fde
Shin <m.shin0509@gmail.com>
2020-02-27 19:01:37 +0900

    delete :size_id: null: false

---
+++ db/migrate/20200225063010_modify_items_properties.rb
@@ -0,0 +1,11 @@
+class ModifyItemsProperties < ActiveRecord::Migration[5.2]
+  def change
+    add_column :items, :category_id_1, :integer, null: false
+    add_column :items, :category_id_2, :integer, null: false
+    add_column :items, :category_id_3, :integer, null: false
+    add_column :items, :size_id, :integer
+    remove_column :items, :shipping, :integer
+    add_column :items, :shipping, :string, null: false
+    add_reference :items, :category, foreign_key: true
+  end
+end
